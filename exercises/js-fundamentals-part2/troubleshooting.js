58f35c1f2c71b1b2069dd78e2f1ebdfdb01e3401
chsykas <dev.chsykas@gmail.com.noreply.github.com>
2023-05-19 04:15:35 +0300

    Exercises js fundmentals part 2

---
+++ exercises/js-fundamentals-part2/troubleshooting.js
@@ -0,0 +1,34 @@
+// function troubleshooting() {
+//   const a = 1;
+//   const b = 1;
+
+//   let result;
+
+//   result = a + b;
+
+//   return result;
+// }
+
+// console.log(troubleshooting(result));
+
+// let number = Number(prompt("Enter a number"));
+
+// function numberChecker() {
+//   if (number >= 10) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// numberChecker();
+
+const firstName = "Christos";
+const lastName = "Sykas";
+const currentYear = 2023;
+const birthYear = 1984;
+const greeting = `Hello my name is ${firstName} ${lastName}, and i am ${
+  currentYear - birthYear
+} years old.`;
+
+console.log(greeting);
