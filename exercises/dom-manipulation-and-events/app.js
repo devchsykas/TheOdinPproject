5611f74a05e66692ef5522bfc297172cb9f7c1b7
chsykas <dev.chsykas@gmail.com.noreply.github.com>
2023-06-27 02:34:17 +0300

    Add eventListeners to the js file

--- exercises/dom-manipulation-and-events/app.js
+++ exercises/dom-manipulation-and-events/app.js
@@ -31,3 +31,37 @@ const para2 = document.createElement("p");
 para2.classList.add("para2");
 para2.textContent = "ME TOO!";
 div.appendChild(para2);
+
+const btn = document.querySelector("#btn");
+// btn.onclick = function () {
+//   alert("Hello World");
+// };
+btn.onclick = alertFunction;
+
+const btn1 = document.querySelector("#btn1");
+// btn1.addEventListener("click", function () {
+//   alert("Hello World");
+// });
+btn1.addEventListener("click", alertFunction);
+
+const btn2 = document.querySelector("#btn2");
+btn2.addEventListener("click", function (e) {
+  console.log(e);
+  console.log(e.target);
+  e.target.style.background = "blue";
+});
+
+function alertFunction() {
+  alert("Hello World");
+}
+
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll("button");
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", function () {
+    alert(button.id);
+  });
+});
