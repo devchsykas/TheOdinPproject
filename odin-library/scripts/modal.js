515fa5b003d5949a84ebfc4a45d32131cde42150
Christos Sykas <dev.chsykas@gmail.com>
2026-04-23 12:27:20 +0300

    `Refactored modal functionality in modal.js`

--- odin-library/scripts/modal.js
+++ odin-library/scripts/modal.js
@@ -1,19 +1,26 @@
-/**
- * The `addBookBtn` and `closeModalBtn` elements are selected from the DOM.
- */
+"use strict";
+
 const addBookBtn = document.querySelector(".add-book--btn");
+const addBookModalBtn = document.getElementById("addBookModal");
 const closeModalBtn = document.getElementById("closeModal");
 
-/* When the `addBookBtn` element is
-clicked, this event listener triggers a function that selects the element with the id "addBookModal" from the DOM and removes the "hidden" class from it. This action likely displays a modal or form that was previously hidden from view. */
-addBookBtn.addEventListener("click", () => {
-  const addBookModal = document.getElementById("addBookModal");
-  addBookModal.classList.remove("hidden");
-});
+function openModal() {
+  addBookModalBtn.classList.remove("hidden");
+}
 
-/* When the `closeModalBtn` is
-clicked, the function inside the event listener is triggered. It selects the element with the id "closeModal" from the DOM and adds the class "hidden" to it. */
-closeModalBtn.addEventListener("click", () => {
-  //When you click the cancel button close the form
-  addBookModal.classList.add("hidden");
-});
+function closeModal() {
+  addBookModalBtn.classList.add("hidden");
+}
+
+function handleEscapeKey(e) {
+  if (e.key === "Escape") {
+    closeModal();
+  }
+}
+
+// console.log(addBookBtn);
+addBookBtn.addEventListener("click", openModal);
+
+closeModalBtn.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", handleEscapeKey);
