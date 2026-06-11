84d86f1d8946708abcfcdd62a162087643fc6435
Christos Sykas <dev.chsykas@gmail.com>
2026-06-11 12:42:41 +0300

    Refactor website initialization

--- odin-restaurant-page/src/index.js
+++ odin-restaurant-page/src/index.js
@@ -1,7 +1,15 @@
 import "/src/assets/styles/styles.css";
+import createNavbar from "./components/navbar.js";
 import createHomePage from "./pages/home.js";
 
-document.addEventListener("DOMContentLoaded", () => {
+function initWebsite() {
   const content = document.getElementById("content");
-  content.appendChild(createHomePage());
-});
+
+  const navbar = createNavbar();
+  content.appendChild(navbar);
+
+  const homePage = createHomePage();
+  content.appendChild(homePage);
+}
+
+initWebsite();
