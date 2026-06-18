2e82487a99685bbb704abc5da5923d0270313c02
Christos Sykas <dev.chsykas@gmail.com>
2026-06-18 13:12:44 +0300

    Add initial menu data structure for restaurant page

---
+++ odin-restaurant-page/src/data/menuData.js
@@ -0,0 +1,16 @@
+const menuData = [
+  {
+    title: "Starters",
+    note: "Small plates, great for sharing.",
+    items: [
+      {
+        name: "",
+        price: "",
+        image: "",
+        description: "",
+      },
+    ],
+  },
+];
+
+export default menuData;
