8d17b94a1a09c9074b976e75eed6042f9599992e
Christos Sykas <dev.chsykas@gmail.com>
2026-08-01 23:18:20 +0300

    feat: import modals styles and module in index.js

--- odin-todo-app/src/index.js
+++ odin-todo-app/src/index.js
@@ -3,5 +3,7 @@ import "./styles/header.css";
 import "./styles/sidebar.css";
 import "./styles/cards.css";
 import "./styles/footer.css";
+import "./styles/modals.css";
+import "./modules/modals.js";
 
 console.log("Hello, World!");
