3f793e61306e5670a5b137f18595f240270fc5e3
Christos Sykas <dev.chsykas@gmail.com>
2026-08-01 20:44:21 +0300

    feat: import additional styles for header, sidebar, cards, and footer

--- odin-todo-app/src/index.js
+++ odin-todo-app/src/index.js
@@ -1,3 +1,7 @@
 import "./styles/main.css";
+import "./styles/header.css";
+import "./styles/sidebar.css";
+import "./styles/cards.css";
+import "./styles/footer.css";
 
 console.log("Hello, World!");
