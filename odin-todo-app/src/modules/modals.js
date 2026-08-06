9697aff665156caa962916b745c3fd4515825af0
Christos Sykas <dev.chsykas@gmail.com>
2026-08-06 12:30:22 +0300

    feat: enhance modal functionality with event listeners for task and project management

--- odin-todo-app/src/modules/modals.js
+++ odin-todo-app/src/modules/modals.js
@@ -2,14 +2,28 @@
  * This module handles the modals for adding tasks and creating projects.
  *
  */
+
+/**
+ * Selects the "Add Project" button and the project modal.
+ * Selects the "Add Task" button and the task modal.
+ */
 const addProjectModalBtn = document.getElementById("btn-add-project-modal");
 const projectModal = document.getElementById("project-modal");
 const projectForm = document.getElementById("project-form");
+const addTaskModalBtn = document.getElementById("btn-add-task-modal");
+const taskModal = document.getElementById("task-modal");
+const taskForm = document.getElementById("task-form");
 
+/**
+ * Adds a click event listener to the "Add Project" button to open the project modal.
+ */
 addProjectModalBtn.addEventListener("click", () => {
   projectModal.showModal();
 });
 
+/**
+ * Adds a close event listener to the project modal to handle the creation of a new project.
+ */
 projectModal.addEventListener("close", () => {
   if (projectModal.returnValue === "create") {
     console.log("Project created");
@@ -17,4 +31,22 @@ projectModal.addEventListener("close", () => {
     console.log("Project canceled");
   }
   projectForm.reset();
+});
+
+/**
+ * Adds a click event listener to the "Add Task" button to open the task modal.
+ */
+addTaskModalBtn.addEventListener("click", () => {
+  taskModal.showModal();
+});
+
+/**
+ * Adds a close event listener to the task modal to handle the creation of a new task.
+ */
+taskModal.addEventListener("close", () => {
+  if (taskModal.returnValue === "save") {
+    console.log("Task created");
+  } else {
+    console.log("Task canceled");
+  }
 });
